0d7a1a4d0573aa6d853e4b70bd92c549693bfe7d
danish1357 <danish.ansari1357@gmail.com>
2016-11-04 17:56:41 +0530

    MainActivity.java and crop scale aspect ratio disabled..

--- app/src/main/java/timefish/com/rings4w/MainActivity.java
+++ app/src/main/java/timefish/com/rings4w/MainActivity.java
@@ -1,43 +1,47 @@
 package timefish.com.rings4w;
 
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
-import com.victor.ringbutton.RingButton;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 
 import gun0912.tedbottompicker.TedBottomPicker;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int CAMERA_REQUEST = 121;
+    private FancyButton fancyButtonStart;
+    private ImageView imageViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RingButton ringButton = (RingButton) findViewById(R.id.ringButton);
-        ringButton.setOnClickListener(new RingButton.OnClickListener() {
+        fancyButtonStart = (FancyButton) findViewById(R.id.btn_start);
+        imageViewLogo = (ImageView) findViewById(R.id.logo);
+        animateLogo();
+//        animateButton();
+        fancyButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void clickUp() {
-                startCamera();
-            }
-
-            @Override
-            public void clickDown() {
+            public void onClick(View view) {
+//                animateButton();
                 startGallery();
             }
         });
+
+    }
+
+    private void animateLogo() {
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.fall);
+        myAnim.setInterpolator(new AccelerateInterpolator());
+        imageViewLogo.startAnimation(myAnim);
 
     }
 
@@ -54,46 +58,20 @@ public class MainActivity extends AppCompatActivity {
         tedBottomPicker.show(getSupportFragmentManager());
     }
 
-    private void startCamera() {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-    }
-
 
     private void startCropImage(Uri imageUri) {
         CropImage.activity(imageUri)
-                .setGuidelines(CropImageView.Guidelines.ON).setCropShape(CropImageView.CropShape.OVAL)
+                .setGuidelines(CropImageView.Guidelines.ON).setCropShape(CropImageView.CropShape.OVAL).setFixAspectRatio(true).setAspectRatio(1, 1)
                 .start(this);
     }
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            startCropImage(getImageUri(this, photo));
-        }
-
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri();
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-            }
-        }
+    void animateButton() {
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        double animationDuration = 2 * 1000;
+        myAnim.setDuration((long) animationDuration);
+        BounceInterpolator interpolator = new BounceInterpolator(0.2f, 20.0f);
+        myAnim.setInterpolator(interpolator);
+        fancyButtonStart.startAnimation(myAnim);
     }
 
 }
